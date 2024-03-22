b92f817c0b55fe33765588d19ba4b64627bf24d4
Natsheh11 <65265026+Natsheh11@users.noreply.github.com>
2024-03-22 13:19:00 +0200

    Update sv_main.cpp
    It should be view entity, so players using special view can hear/see effects

--- rehlds/engine/sv_main.cpp
+++ rehlds/engine/sv_main.cpp
@@ -880,7 +880,7 @@ qboolean SV_ValidClientMulticast(client_t *client, int soundLeaf, int to)
 		return TRUE;
 	}
 
-	int bitNumber = SV_PointLeafnum(client->edict->v.origin);
+	int bitNumber = SV_PointLeafnum(client->pViewEntity->edict->v.origin);
 	if (mask[(bitNumber - 1) >> 3] & (1 << ((bitNumber - 1) & 7)))
 	{
 		return TRUE;
